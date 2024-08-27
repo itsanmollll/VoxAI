4b8d452115f038d052685127c3929c67643989ad
Anmol Bhardwaj <anmolbhardwajinv@gmail.com>
2024-08-27 12:03:15 +0530

    /

--- src/voicetotext.py
+++ src/voicetotext.py
@@ -19,3 +19,18 @@ result = whisper.decode(model, mel, options)
 
 # print the recognized text
 print(result.text)
+
+
+
+# WHISPER EXAMPLE from Transformers
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("automatic-speech-recognition", model="openai/whisper-large-v3")
+
+
+# Load model directly
+from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
+
+processor = AutoProcessor.from_pretrained("openai/whisper-large-v3")
+model = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-large-v3")
